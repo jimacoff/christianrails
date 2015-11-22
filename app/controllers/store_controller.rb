ed7db320c5b26249accc0ac65869672701977f92
Christian DeWolf <christian@wolfbutler.com>
2015-11-22 18:52:26 -0400

    Some reorganization

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
 
+  def index
+    @price_combos = PriceCombo.all
+    @owned_products = []
+
+    if current_user
+      @owned_products = current_user.products
+    end
+
+    @available_products = @all_products - @owned_products
+  end
+
+  def admin
+  end
+
   def updated_prices
     if current_user
       price_json = {}
@@ -17,9 +31,30 @@ class StoreController < ApplicationController
   end
 
   def check_out
-    
+    # TODO checkout function    
 
 
+  end
+
+  def download(release_id)
+    if current_user
+      begin
+        release = Release.find(release_id)
+      rescue
+        Rails.logger.warn("Ebook download attempted on invalid release id: #{release_id} by user id: #{current_user.id}.")
+        return
+      end
+      product = release.product
+      if current_user.has_product?(product_id)
+        send_file "#{Rails.root}/../../downloads/#{release.file_name}"
+        Download.create(user: current_user, release: release)
+        return
+      else
+        Rails.logger.warn("Ebook download attempted on unauthorized product id: #{product.id} by user id: #{current_user.id}.")
+        return
+      end
+    end
+    Rails.logger.warn("Unauthorized download attempted on release: #{release_id} by a guest user.")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,25 @@
 Christianrails::Application.routes.draw do
   
-  resources :staged_purchases
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  resources :downloads,     only:   [:create]
-  resources :purchases,     only:   [:index, :create]
-  resources :releases,      except: [:show, :new, :edit, :index]
-  resources :price_combos,  except: [:show]
-  resources :products,      except: [:show]
+  resources :staged_purchases,     only:   [:index, :create, :destroy]
+  resources :purchases,            only:   [:index, :create]
+  resources :releases,             except: [:index, :show, :new, :edit, ]
+  resources :price_combos,         except: [:show]
+  resources :products,             except: [:show]
   
   resources :store,  only: [:index] do
     collection do
+      get 'admin'
       get 'updated_prices'
       get 'check_out'
-    end
-  end
-
-  resources :main, only: [] do
-    collection do
-      get 'store'
+      get 'download'
     end
   end
   
-  root 'main#index'
-  get '/admin', to: 'main#admin'
+  get '/admin', to: 'store#admin'
   get '/store', to: 'store#index'
-
-  #get '/downloads', to: 'downloads#test'
+  
+  root 'store#index'
 
 end
