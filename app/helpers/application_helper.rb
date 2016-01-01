4a1f112e3a4b54bb808a78a0c22159911f2c22e9
Christian DeWolf <christian@wolfbutler.com>
2016-01-01 14:25:26 -0400

    Add version to view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     current_user.nil? ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
   end
 
+  def app_version
+    yaml_file_path = "#{::Rails.root}/config/version.yml"
+    parts = YAML.load(File.read(yaml_file_path)).symbolize_keys
+    "#{parts[:major]}.#{parts[:minor]}.#{parts[:patch]}"
+  end
+
 end
