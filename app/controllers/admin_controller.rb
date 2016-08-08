7dd4d03581951c8580737934e10bfb003f7fba5c
Christian DeWolf <christian@wolfbutler.com>
2016-08-08 18:30:19 -0300

    More on mailer

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,8 +7,8 @@ class AdminController < ApplicationController
     @failure = false
     begin
       @contact = Crm::Contact.find(3) # me!
-      NewsletterMailer.welcome(@contact).deliver_later
-      NewsletterMailer.welcome(@contact).deliver
+      # NewsletterMailer.welcome(@contact).deliver_later
+      NewsletterMailer.welcome(@contact).deliver_now
     rescue
       @failure = true
     end
