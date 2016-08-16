557c6c3dd1eeb2da4ea0e8989d3b91465314ff8d
Christian DeWolf <christian@wolfbutler.com>
2016-08-16 20:47:23 -0300

    More time styling

--- app/helpers/crm_helper.rb
+++ app/helpers/crm_helper.rb
@@ -12,15 +12,14 @@ module CrmHelper
     current_user.assistant if current_user && current_user.assistant
   end
 
-  def relative_time(datetime)
-      # TODO modify to show time when flag given
+  def relative_time(datetime, time = false)
     return "Today"    if datetime.strftime("%a, %b %e") == (DateTime.now).strftime("%a, %b %e")
     return "Tomorrow" if datetime.strftime("%a, %b %e") == (DateTime.now + 1.day).strftime("%a, %b %e")
     if datetime < Time.now.to_datetime  # in the past!
       return "Yesterday" if datetime.strftime("%a, %b %e") == (DateTime.now - 1.day).strftime("%a, %b %e")
       return ((DateTime.now.to_i - datetime.to_i) / 86400).to_s + " days ago"
     end
-    datetime.strftime("%a, %b %e")
+    time ? datetime.strftime("%a, %b %e, %-I:%M %p") : datetime.strftime("%a, %b %e")
   end
 
 end
