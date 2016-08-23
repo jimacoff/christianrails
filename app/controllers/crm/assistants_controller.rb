b1fd1fe100cb5ad0f019b0541f9cea443787bb9b
Christian DeWolf <christian@wolfbutler.com>
2016-08-23 10:43:37 -0300

    Extra @

--- app/controllers/crm/assistants_controller.rb
+++ app/controllers/crm/assistants_controller.rb
@@ -116,7 +116,7 @@ class Crm::AssistantsController < Crm::CrmController
                                           .where(type_id: Crm::Mailout::TYPE_DAILY_SUMMARY)
                                           .where('created_at > ?', DateTime.now - 1.day)
       unless mailouts_to_assistant.size > 0
-        Crm::ReminderMailer.daily_summary( @assistant ).deliver_now
+        Crm::ReminderMailer.daily_summary( assistant ).deliver_now
         create_new_mailout_record( assistant, Crm::Mailout::TYPE_DAILY_SUMMARY )
       end
     end
