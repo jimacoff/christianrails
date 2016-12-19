e8dfe3db7fcdf1eab8d90f4ae91facbd7a2cf5b8
Christian DeWolf <christian@wolfbutler.com>
2016-12-18 23:58:20 -0400

    Custom 500 page for Diamond Find

--- app/controllers/woods/woods_controller.rb
+++ app/controllers/woods/woods_controller.rb
@@ -1,3 +1,11 @@
 class Woods::WoodsController < ApplicationController
   layout "binarywoods"
+
+  rescue_from Exception, with: :render_error
+
+  private
+
+    def render_error
+      render '/woods/errors/server_error', status: 500
+    end
 end
