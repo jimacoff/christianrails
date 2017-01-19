e54c9dc971f470430f2cc6c54f797e0381f01974
Christian DeWolf <christian@wolfbutler.com>
2017-01-18 22:49:31 -0400

    Fix security lol

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Christianrails::Application.config.secret_key_base = '<%= ENV["CHRISTIANRAILS_SECRETTOKEN"] %>'
+Christianrails::Application.config.secret_key_base = ENV["CHRISTIANRAILS_SECRETTOKEN"]
