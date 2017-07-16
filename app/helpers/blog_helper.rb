297b0a8037ec0f07a5c6d95c432ef837f994bffc
Christian DeWolf <christian@wolfbutler.com>
2017-07-16 14:04:29 -0300

    New store blog post re: Snapback vol2

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,62 +1,73 @@
 module BlogHelper
 
+  # categories
+  WRITING_AND_PUBLISHING  = "Writing and Publishing"
+  RARE_PUBLIC_APPEARANCES = "Rare Public Appearances"
+  SOFTWARE_DEVELOPMENT    = "Software Development"
+  FREELANCE_LIFE          = "Freelance Life"
+
   def all_blog_posts
     # Add new posts at the top.
     [
       {
+        title: "20170716_on_writing_novellas",
+        category: WRITING_AND_PUBLISHING,
+        tags: ["Snapback", "novellas", "illustrations", "Hal-Con", "Go", "self-publishing"]
+      },
+      {
         title: "20170402_a_dream_of_spring",
-        category: "Rare Public Appearances",
-        tags: ["Geequinox", "Convention", "self-publishing", "Go", "Hal-Con"]
+        category: RARE_PUBLIC_APPEARANCES,
+        tags: ["Geequinox", "conventions", "self-publishing", "Go", "Hal-Con"]
       },
       {
         title: "20161201_a_snapback_review",
-        category: "Writing and Publishing",
+        category: WRITING_AND_PUBLISHING,
         tags: ["Snapback", "self-publishing", "Go", "reviews", "Ghostcrime"]
       },
       {
         title: "20161113_snapback_released",
-        category: "Writing and Publishing",
+        category: WRITING_AND_PUBLISHING,
         tags: ["Snapback", "self-publishing", "Go", "Hal-Con", "deals"]
       },
       {
         title: "20161022_snapback_to_the_printers",
-        category: "Writing and Publishing",
+        category: WRITING_AND_PUBLISHING,
         tags: ["Snapback", "Hal-Con", "self-publishing", "Go", "e-commerce"]
       },
       {
         title: "20160909_word_on_the_street",
-        category: "Rare Public Appearances",
+        category: RARE_PUBLIC_APPEARANCES,
         tags: ["Word on the Street", "Halifax", "self-publishing", "Ghostcrime", "badgers"]
       },
       {
         title: "20160816_tie_in_for_ghostcrime",
-        category: "Software Development",
+        category: SOFTWARE_DEVELOPMENT,
         tags: ["CRM", "Ghostcrime", "organization", "business", "books", "tie-ins"]
       },
       {
         title: "20160808_between_contracts",
-        category: "Freelance Life",
+        category: FREELANCE_LIFE,
         tags: ["freelance", "self-employment", "travelling", "British Columbia", "Snapback", "books"]
       },
       {
         title: "20160618_new_book_in_the_works",
-        category: "Writing and Publishing",
-        tags: ["Hal-Con", "Snapback", "Go", "Novella", "Illustration"]
+        category: WRITING_AND_PUBLISHING,
+        tags: ["Hal-Con", "Snapback", "Go", "novellas", "illustrations"]
       },
       {
         title: "20160522_ghostcrime_2ed_printed",
-        category: "Writing and Publishing",
+        category: WRITING_AND_PUBLISHING,
         tags: ["Ghostcrime", "Hal-Con", "Self-publishing"]
       },
       {
         title: "20160501_spring_cleaning",
-        category: "Rare Public Appearances",
-        tags: ["Ghostcrime", "Geequinox", "Convention"]
+        category: RARE_PUBLIC_APPEARANCES,
+        tags: ["Ghostcrime", "Geequinox", "conventions"]
       },
       {
         title: "20160411_christian_at_geequinox",
-        category: "Rare Public Appearances",
-        tags: ["Ghostcrime", "Geequinox", "Convention"]
+        category: RARE_PUBLIC_APPEARANCES,
+        tags: ["Ghostcrime", "Geequinox", "conventions"]
       }
     ]
   end
