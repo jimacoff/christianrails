42e90efbf528d89b2720631bb1f13728fd6d4297
Christian DeWolf <christian@wolfbutler.com>
2017-07-18 21:48:51 -0300

    Refactor

--- app/helpers/crm_helper.rb
+++ app/helpers/crm_helper.rb
@@ -1,4 +1,5 @@
 module CrmHelper
+  include LogHelper
 
   def owns_assistant?(assistant)
     current_user && current_user.assistant && (current_user.assistant == assistant)
@@ -25,5 +26,31 @@ module CrmHelper
   def tack_on_time(include_time, datetime)
     include_time ? " at #{datetime.strftime("%-I:%M %p")}" : ""
   end
+
+  def send_out_daily_emails
+    Crm::Assistant.where(email_me_daily: true).each do |assistant|
+      if assistant.ripe_for_email?
+        send_daily_summary( assistant )
+      end
+    end
+  end
+
+  private
+
+    def send_daily_summary(assistant)
+      attempts = 3
+      begin
+        Crm::ReminderMailer.daily_summary( assistant ).deliver_now
+        create_new_mailout_record( assistant, Crm::Mailout::TYPE_DAILY_SUMMARY )
+      rescue SocketError
+        record_warning(Log::CRM, "Socket Error sending email for #{assistant.name}.")
+        sleep 3
+        retry if (attempts -= 1) >= 0
+      end
+    end
+
+    def create_new_mailout_record(assistant, type)
+      Crm::Mailout.create(assistant_id: assistant.id, type_id: type, status_id: Crm::Mailout::STATUS_COMPLETE)
+    end
 
 end
