ab97821ae033d1977b908ff53ee6e8cc64fe71bc
Christian DeWolf <christian@wolfbutler.com>
2017-07-27 23:24:00 -0300

    More improvements to new cart + main page; update GC crm add link

--- app/controllers/store/ghostcrime_controller.rb
+++ app/controllers/store/ghostcrime_controller.rb
@@ -18,6 +18,17 @@ class Store::GhostcrimeController < Store::StoreController
     end
 
     @available_products = @all_products - @owned_products
+
+    # for auto-adding of GC from the params
+    @gc_product = Store::Product.where(title: "Ghostcrime").first
+    @gc_crm = ""
+    if @gc_product && current_user && params[:gc] == "crm"
+      if @available_products.collect{ |x| x.id }.include?( @gc_product.id )
+        @gc_crm = "add-to-cart"
+        record_positive_event(Log::STORE, "Ghostcrime added to cart from CRM")
+      end
+    end
+
   end
 
 end
