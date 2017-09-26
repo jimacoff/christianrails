a8c935096b7328745f59eee781e419fc77a0515a
Christian DeWolf <christian@wolfbutler.com>
2017-09-25 22:09:12 -0300

    In progress popper

--- app/assets/javascripts/scalequail/quail.js
+++ app/assets/javascripts/scalequail/quail.js
@@ -1,4 +1,18 @@
-// pops a notification that the post is coming soon
-function comingSoon() {
-  console.log("Coming soon!");
+// pops a notification that the post is coming soon.
+// takes the link it was clicked on.
+function comingSoon( clicked ) {
+  popWidth = $('.scaling-in-progress').width();
+  popOffset = 190;
+  $('.scaling-in-progress').css("margin-top",   (clicked[0].offsetTop - popOffset) + 'px');
+  $('.scaling-in-progress').css("margin-left",  (window.innerWidth - popWidth) / 2 + 'px');
+  $('.scaling-in-progress').css("margin-right", (window.innerWidth - popWidth) / 2 + 'px');
+  $('.scaling-in-progress').fadeIn();
+  window.setTimeout(function(){
+    hideComingSoonBox();
+  }, 2500);
+}
+
+
+function hideComingSoonBox() {
+  $('.scaling-in-progress').fadeOut();
 }
