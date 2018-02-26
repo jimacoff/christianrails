e7981ea399dc4a88e8e072220f8fdca3b948afa3
Christian DeWolf <christian@wolfbutler.com>
2018-02-25 22:52:00 -0400

    Update to the Computers blog which I forgot about

--- app/helpers/computers_helper.rb
+++ app/helpers/computers_helper.rb
@@ -4,11 +4,11 @@ module ComputersHelper
     # Add new posts at the top.
     [
       {
-        slug: "20170527_git_for_authors",
+        slug: "20180225_backing_up_your_novel",
         title: "Backing up your novel the right way",
         category: "Tutorials",
         tags: ["git", "version control", "raspberry pi", "backups", "ransomware"],
-        desc: "What author needs version control? ALL OF THEM"
+        desc: "Protect your novel from theft, fire and computer failure, or one of those will definitely happen."
       }
     ]
   end
