f6ae9a9c83d42a81f6d4cda02e39d1e18366d760
Christian DeWolf <christian@wolfbutler.com>
2018-09-03 20:19:14 -0300

    Some basic navigation for steveOS

--- app/assets/javascripts/hackersteve/steve.js
+++ app/assets/javascripts/hackersteve/steve.js
@@ -1,10 +1,34 @@
 // HACKER STEVE in... DEANHACK 5000
 
+var currentLocation = 'programDirectory';
+
+var npcs = {
+  eugene: {name: "Eugene"},
+  gerald: {name: "Gerald"}
+}
+
 var programs = {
   email:     { installed: true, icon: "envelope", link: "email",     name: "SteveMail" },
-  browser:   { installed: true, icon: "envelope", link: "browser",   name: "Browser" },
-  networker: { installed: true, icon: "envelope", link: "networker", name: "Network Expolorer" },
-  console:   { installed: true, icon: "envelope", link: "console",   name: "Console" }
+  browser:   { installed: true, icon: "eye",      link: "browser",   name: "Browser" },
+  networker: { installed: true, icon: "cloud",    link: "networker", name: "Network Explorer" },
+  console:   { installed: true, icon: "chevron",  link: "console",   name: "Console" }
+};
+
+var files = {
+  privatekey: {owned: true, icon: "lock", link: "access-denied", name: "Steve's Private Key"},
+};
+
+var interfaces = {
+  programDirectory: "steveOS",
+  fileDirectory:    "steveOS",
+  email:            "email",
+  browser:          "browser",
+  networker:        "networker",
+  console:          "console",
+};
+
+var emails = {
+  coffeeMachineBroken: { received: true, subject: "", content: "", from: npcs[ 'gerald' ] }
 }
 
 ///// DISPLAY HELPERS /////
@@ -12,6 +36,7 @@ var programs = {
 function makeIcon(name, icon, link) {
   var newIcon = document.createElement('span');
   newIcon.className = "steveOS-icon";
+
   // draw picture
   var iconImage = document.createElement('img');
   iconImage.className = "steveOS-icon-image";
@@ -34,8 +59,10 @@ function makeIcon(name, icon, link) {
   return newIcon;
 }
 
-function drawSteveOS() {
-  directoryContainer = document.getElementsByClassName('steveOS-directory')[0];
+function drawSteveOSProgramDirectory() {
+  var directoryContainer = document.createElement('div');
+  directoryContainer.className = "steveOS-directory";
+
   // draw each installed program
   Object.keys( programs ).forEach( function(program) {
     let theProgram = programs[program];
@@ -45,15 +72,72 @@ function drawSteveOS() {
   });
 
   // draw icon for files folder
-  directoryContainer.appendChild( makeIcon('Files', 'envelope', 'files' ) );
+  directoryContainer.appendChild( makeIcon('Files', 'folder', 'fileDirectory' ) );
+
+  attachToProgramContainer( directoryContainer );
+}
+
+function drawSteveOSFileDirectory() {
+  var directoryContainer = document.createElement('div');
+  directoryContainer.className = "steveOS-directory";
+
+  Object.keys( files ).forEach( function(file) {
+    let theFile = files[file];
+    if (theFile.owned) {
+      directoryContainer.appendChild( makeIcon(theFile.name, theFile.icon, theFile.link ) );
+    }
+  });
+
+  attachToProgramContainer( directoryContainer );
+  attachToProgramContainer( homeButtonBar() );
+}
+
+function attachToProgramContainer( element ) {
+  programContainer = document.getElementsByClassName('program-container')[0];
+  programContainer.appendChild( element );
+}
+
+// wipe the whole program container, because it's that kind of OS
+function clearScreen() {
+  programContainer = document.getElementsByClassName('program-container')[0];
+  while (programContainer.hasChildNodes()) {
+    programContainer.removeChild(programContainer.lastChild);
+  }
+}
+
+function homeButtonBar() {
+  var homeButtonContainer = document.createElement('div');
+  homeButtonContainer.className = "home-button-container";
+
+  var homeButton = document.createElement('span');
+  homeButton.className = "steveOS-icon";
+
+  var homeIconImage = document.createElement('img');
+  homeIconImage.className = "steveOS-icon-image";
+  homeIconImage.src = "/assets/hackersteve/icon-home.png";
+
+  homeIconImage.addEventListener("click", function(event) {
+    followLink('home');
+    event.preventDefault();
+  });
+
+  homeButton.appendChild(homeIconImage);
+  homeButtonContainer.appendChild(homeButton);
+
+  return homeButtonContainer;
 }
 
 ///// NAVIGATION HELPERS /////
 
 function followLink( link ) {
   console.log('following link to ' + link);
-  // TODO
+  clearScreen();
 
+  if (link === "fileDirectory") {
+    drawSteveOSFileDirectory();
+  } else if (link === "home") {
+    drawSteveOSProgramDirectory();
+  }
 }
 
 ///// INITIALIZERS /////
@@ -61,5 +145,5 @@ function followLink( link ) {
 // initialize the game
 $(document).ready(function() {
   console.log('Initializing steveOS');
-  drawSteveOS();
+  drawSteveOSProgramDirectory();
 });
